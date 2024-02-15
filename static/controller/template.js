22de93e302fbb3b57ae943e07953445f5e915a44
HeroBuxx <me@herobuxx.me>
2024-02-15 09:35:28 +0700

    rps: Tambah URL update Pokok Bhasan
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- static/controller/template.js
+++ static/controller/template.js
@@ -1,3 +1,4 @@
 // Endpoint Get
 export let GetAllMatkul = "https://mrt.ulbi.ac.id/dosen/matkul";
 export let GetRpsByKodeMK = "https://mrt.ulbi.ac.id/dosen/rps/get/";
+export let PutPokokBahasan = "https://mrt.ulbi.ac.id/dosen/rps";
